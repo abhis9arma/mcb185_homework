49862b54f6cf816b105d2dd75d3e01b3d2770da7
username <abhis9arma@gmail.com>
2024-01-22 15:11:53 -0800

    fixed

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -1,4 +1,4 @@
 cd ~/Code/MCB185/data
-gunzip -c dictionary.gz| grep -E "^[zoniarc]*r[zoniarc]*[zoniarc]$" | grep -v "(1,3)$"
+gunzip -c dictionary.gz| grep -E "^[zoniarc]*r[zoniarc]*$" | grep .... 
 
-##I keep getting words with three letters
+

12d48eadbd1d95c09af0fd0c1223d4dc1613db23
username <abhis9arma@gmail.com>
2024-01-23 09:17:54 -0800

    quiz

--- 20demo.py
+++ 20demo.py
@@ -9,3 +9,11 @@ multi line
 comment
 """
 
+print(1 + 1)
+print(1 - 1)
+print(2 * 2)
+print(1 / 2)
+print(2 ** 3)
+print(5 // 2)
+print(5 % 2)
+print(5 * (2 + 1)) 
--- quiz1.sh
+++ quiz1.sh
@@ -1,3 +1,7 @@
 echo "Abhi Sharma"
 echo "user : $USER"
 
+
+cd ~/Code/MCB185/data
+gunzip -c dictionary.gz| grep -E "^[acuomtf]*a[acuotmf]*$" | grep .... 
+

f6ab4065ec01993d2d43526c1c8dec8a6b2125cc
username <abhis9arma@gmail.com>
2024-02-06 09:19:49 -0800

    Q2

--- 38quiz.py
+++ 38quiz.py
@@ -18,4 +18,28 @@ def gregory(n):
 print(gregory(2))
 print(gregory(1000))
 
-#
+#Task 2 Gregory-Leibniz and Nilakantha
+
+def pi(n):
+	def gregory(n):
+		sign = -1
+		result = 0
+		for i in range(1, n):
+			x = sign * (1 / (i * 2 + 1))
+			result = x + result
+			sign = sign * -1
+		total = 1 + result
+		pi = total * 4
+		return pi
+	def nilakantha(n):
+		sign = 1
+		result = 0
+		for i in range(1, n):
+			x = sign * (4 / ((i * 2) * (i * 2 + 1) * (i * 2 + 2)))
+			result = x + result
+			sign = sign * -1
+		final = 3 + result
+		return final
+	return(gregory(n), nilakantha(n))
+
+print(pi(100))

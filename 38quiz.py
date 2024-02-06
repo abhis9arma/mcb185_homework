81f4da9fea4dd6d26254a0d7524c9999e6c4a189
username <abhis9arma@gmail.com>
2024-02-06 09:11:56 -0800

    Q1

--- 38quiz.py
+++ 38quiz.py
@@ -1,4 +1,7 @@
-# Task 1 Gregory-Leibniz by Abhi Sharma and Connor Suen
+# Abhi Sharma and Connor Suen
+
+
+#Task 1 Gregory-Leibniz
 
 def gregory(n):
 	sign = -1
@@ -7,13 +10,12 @@ def gregory(n):
 		x = sign * (1 / (i * 2 + 1))
 		result = x + result
 		sign = sign * -1
-	final = 1 - result
+	final = 1 + result
 	pi = final * 4
 	print(pi)
 	return final
 		
-
-
-
-print(gregory(3))
+print(gregory(2))
 print(gregory(1000))
+
+#

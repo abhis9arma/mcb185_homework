6b29b350c12041a8d5ed7f4e080daf89d83a8754
username <abhis9arma@gmail.com>
2024-02-10 21:29:42 -0800

    Unit 3

--- 31fizzbuzz.py
+++ 31fizzbuzz.py
@@ -4,9 +4,9 @@ limit = 100
 for x in range(1, limit):
 	if x % 3 == 0:
 		print("Fizz")
-	if x % 5 == 0:
+	elif x % 5 == 0:
 		print("Buzz")
-	if x % 3 == 0 and x % 5 == 0:
+	elif x % 3 == 0 and x % 5 == 0:
 		print("FizzBuzz")
 	elif x % 3 != 0 and x % 5 != 0:
 		print(x)

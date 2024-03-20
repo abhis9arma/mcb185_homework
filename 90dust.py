8a8e0c4f8c9c93d0df57461a1b21b98defc25378
username <abhis9arma@gmail.com>
2024-03-20 00:15:34 -0700

    9

---
+++ 90dust.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+
+"""
+parser = argparse.ArgumentParser(description='DNA entropy filter.')
+parser.add_argument('file', type=str, help='name of fasta file')
+parser.add_argument('size', type=int, help='window size')
+parser.add_argument('entropy', type=float, help='entropy threshold')
+arg = parser.parse_args()
+print('dusting with', arg.file, arg.size, arg.entropy)
+
+"""
+
+parser = argparse.ArgumentParser(description='DNA entropy filter.')
+parser.add_argument('file', type=str, help='name of fasta file')
+parser.add_argument('-s', '--size', type=int, default=20,
+    help='window size [%(default)i]')
+parser.add_argument('-e', '--entropy', type=float, default=1.4,
+    help='entropy threshold [%(default).3f]')
+parser.add_argument('--lower', action='store_true', help='soft mask')
+arg = parser.parse_args()
+print('dusting with', arg.file, arg.size, arg.entropy, arg.lower)
+
+
+
+

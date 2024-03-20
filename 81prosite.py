b7046b051cba5709dc7f06cc246092e12e69fdc5
username <abhis9arma@gmail.com>
2024-03-20 00:17:27 -0700

    8

--- 81prosite.py
+++ 81prosite.py
@@ -16,4 +16,4 @@ for defline, seq in mcb185.read_fasta(sys.argv[1]):
 """
 
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
-	if re.search("C.{2,4}C.{3}[LIVMFYWC].{8}H.{3,5}H", seq): print(defline)
+	if re.search("C.{2, 4}C.{3}[LIVMFYWC].{8}H.{3, 5}H", seq): print(defline)
